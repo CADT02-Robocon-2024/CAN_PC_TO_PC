c953cd62df1a2683295e790580e817f71697dac0
Zeinsss <tithyautey@gmail.com>
2024-07-05 18:55:27 +0700

    Test1

--- motors/pc_to_pc.py
+++ motors/pc_to_pc.py
@@ -1,22 +1,14 @@
 import com.can as com
 
-def send_data(mid, data):
+def send_data_can(mid):
     m_data = []
     m_data.append(0xAA)
     m_data.append(0xC8)
-    m_data.append(0x00+ mid)
+    m_data.append(0x00 + mid)
     m_data.append(0x01)
-    for i in range(4, 12):
+    m_data.append(0x01)
+    for i in range(5, 12):
         m_data.append(0x00)
     m_data.append(0x55)
-    com.uca.frame_send(data)
+    com.uca.frame_send(m_data)
     print("Data sent")
-def receive_data():
-    while True:
-        frame = com.uca.frame_receive()
-        
-        if frame:
-            print("Data received")
-            print(frame)
-        else:
-            print("No data received")
