cf21d6983e21f380d246738b078a37c9c5000a75
Zeinsss <tithyautey@gmail.com>
2024-07-08 11:00:30 +0700

    create function to convert data to hex depend on input

--- motors/pc_to_pc.py
+++ motors/pc_to_pc.py
@@ -1,12 +1,23 @@
 import com.can as com
 
-def send_data_can(mid):
+def silo_data_convert(data):
+    if data == 1:
+        return 0x01
+    elif data == 2:
+        return 0x02
+    elif data == 3:
+        return 0x03
+    elif data == 4:
+        return 0x04
+    elif data == 5:
+        return 0x05
+def send_data_can(mid, data):
     m_data = []
     m_data.append(0xAA)
     m_data.append(0xC8)
     m_data.append(0x00 + mid)
     m_data.append(0x01)
-    m_data.append(0x01)
+    m_data.append(silo_data_convert(data))
     for i in range(5, 12):
         m_data.append(0x00)
     m_data.append(0x55)
